030803a3bbdfa059786b6e43f9bbf0826c80562e
Collins <mutaic84@gmail.com>
2022-04-19 18:40:48 +0300

    create own version of map function

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-function myEach(collection, callback) {
+function makeArray(collection) {
     let workingData
     if(typeof(collection) === "object"){
         workingData = Object.values(collection)
     }else {
         workingData = collection
     }
+    return workingData
+}
+
+function myEach(collection, callback) {
+    const workingData = makeArray(collection)
     for(let i = 0; i < workingData.length; i++){
         callback(workingData[i])
     }
     return collection
 }
+
+function myMap(collection, callback) {
+    const workingData = makeArray(collection)
+    let outputArray = []
+    for(let i = 0; i < workingData.length; i++){
+        const newVal = callback(workingData[i])
+        outputArray.push(newVal)
+    }
+    return outputArray
+}
+
